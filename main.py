1b119490bf40e89756f3bc7e911a9826e49631da
Renat2492 <125185019+Renat2492@users.noreply.github.com>
2023-02-19 12:31:58 +0400

    Обновить main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ select = 0
 
 def on_gesture_shake():
     global select
-    select = Math.random_range(1, 3)
+    select = Math.random_range(1, 5)
     if select == 1:
         basic.show_icon(IconNames.SMALL_SQUARE)
     elif select == 2:
         basic.show_icon(IconNames.SQUARE)
     elif select == 3:
         basic.show_icon(IconNames.SCISSORS)
+    elif select == 4:
+        basic.show_icon(IconNames.GHOST)
+    elif select == 4:
+        basic.show_icon(IconNames.SMALL_DIAMOND)
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 let select = 0
 input.onGesture(Gesture.Shake, function on_gesture_shake() {
     
-    select = Math.randomRange(1, 3)
+    select = Math.randomRange(1, 5)
     if (select == 1) {
         basic.showIcon(IconNames.SmallSquare)
     } else if (select == 2) {
         basic.showIcon(IconNames.Square)
     } else if (select == 3) {
         basic.showIcon(IconNames.Scissors)
+    } else if (select == 4) {
+        basic.showIcon(IconNames.Ghost)
+    } else if (select == 4) {
+        basic.showIcon(IconNames.SmallDiamond)
     }
     
 })
